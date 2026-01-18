2dc7a872d533a9ec9bf915fe260a0cc7fb8841d8
Justine Harmon <Jstnharmon02@gmail.com>
2026-01-18 17:48:20 +0300

    Disable laravel error view resolution for serverless (Vercel)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,9 +3,10 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Exceptions\RegisterErrorViewPaths;
 use Throwable;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -21,3 +22,12 @@ return Application::configure(basePath: dirname(__DIR__))
             }
         });
     })->create();
+
+$app->resolving(RegisterErrorViewPaths::class, function ($service) {
+    if (app()->environment('production')) {
+        // Prevent Laravel from attempting to load error Blade views
+        $service->paths = [];
+    }
+});
+
+return $app;
