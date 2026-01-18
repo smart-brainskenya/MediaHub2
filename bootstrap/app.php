6d5dd82f155805cffaa3516d6e619d78e44931c7
Justine Harmon <Jstnharmon02@gmail.com>
2026-01-18 18:06:14 +0300

    new commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Exceptions\RegisterErrorViewPaths;
 use Throwable;
 
+// Determine environment using raw PHP to avoid container calls during bootstrap
+$isProduction = ($_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? 'production') === 'production';
+
 $app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,15 +18,15 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (Throwable $e) {
-            if (app()->environment('production')) {
+    ->withExceptions(function (Exceptions $exceptions) use ($isProduction): void {
+        $exceptions->render(function (Throwable $e) use ($isProduction) {
+            if ($isProduction) {
                 return response('Server Error', 500);
             }
         });
     })->create();
 
-if ($app->environment('production')) {
+if ($isProduction) {
     $app->bind(RegisterErrorViewPaths::class, function () {
         return new class {
             public function __invoke() {
